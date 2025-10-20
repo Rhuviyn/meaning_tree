94a47a0387b44f895b71817bacba51dc93fad246
Rhuviyn <rhuviyn@mail.ru>
2025-10-20 19:50:37 +0300

    feat(PythonLanguage): добавлен учёт параметров sep и end при создании mt узла функции print()

--- modules/languages/python/src/main/java/org/vstu/meaningtree/languages/PythonParser.java
+++ modules/languages/python/src/main/java/org/vstu/meaningtree/languages/PythonParser.java
@@ -404,6 +404,8 @@ public class PythonParser extends LanguageParser {
 
         List<Expression> exprs = new ArrayList<>();
         TSNode arguments = node.getChildByFieldName("arguments");
+        Expression printSep = StringLiteral.fromUnescaped(" ", StringLiteral.Type.NONE);
+        Expression printEnd = StringLiteral.fromUnescaped("\n", StringLiteral.Type.NONE);
         if (arguments.getType().equals("generator_expression")) {
             exprs.add((Expression) parseTSNode(arguments));
         } else {
@@ -412,6 +414,15 @@ public class PythonParser extends LanguageParser {
                 if (tsNodeChildType.equals("(") || tsNodeChildType.equals(")") || tsNodeChildType.equals(",") || tsNodeChildType.equals("comment")) {
                     continue;
                 }
+                if (getCodePiece(tsNode).equals("print") && tsNodeChildType.equals("keyword_argument")) {
+                    if (getCodePiece(arguments.getNamedChild(i).getChildByFieldName("name")).equals("sep")) {
+                        printSep = (Expression) parseTSNode(arguments.getNamedChild(i).getChildByFieldName("value"));
+                        continue;
+                    } else if (getCodePiece(arguments.getNamedChild(i).getChildByFieldName("name")).equals("end")) {
+                        printEnd = (Expression) parseTSNode(arguments.getNamedChild(i).getChildByFieldName("value"));
+                        continue;
+                    }
+                }
                 Expression expr = (Expression) parseTSNode(arguments.getNamedChild(i));
                 exprs.add(expr);
             }
@@ -419,8 +430,8 @@ public class PythonParser extends LanguageParser {
 
         if (getCodePiece(tsNode).equals("print")) {
             return new PrintValues.PrintValuesBuilder()
-                    .endWithNewline()
-                    .separateBySpace()
+                    .endWith(printEnd)
+                    .separateBy(printSep)
                     .setValues(exprs)
                     .build();
         }
